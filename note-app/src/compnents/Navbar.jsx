11e6c25df44099b22d9ccf933ada24dd08502358
Menuka7865 <menukasandaruwan456@gmail.com>
2025-03-19 21:15:00 +0530

    Home page updated

--- note-app/src/compnents/Navbar.jsx
+++ note-app/src/compnents/Navbar.jsx
@@ -1,37 +1,40 @@
-import React, { useState } from 'react'
-import Profileinfo from './Profileinfo'
-import { useNavigate } from 'react-router-dom'
-import Searchbar from './Searchbar'
+import React, { useState } from 'react';
+import Profileinfo from './Profileinfo';
+import { useNavigate } from 'react-router-dom';
+import Searchbar from './Searchbar';
 
 const Navbar = () => {
-   const [searchQuery, setSearchQuery] = useState("")
-   const navigate = useNavigate
-   const onLogout = () =>{
-    navigate('/login')
+   const [searchQuery, setSearchQuery] = useState("");
+   const navigate = useNavigate();
+
+   const onLogout = () => {
+      navigate('/login');
    };
-   const handleSearch = () => {};
+
+   const handleSearch = () => {
+      console.log("Searching for:", searchQuery);
+   };
+
    const onClearSearch = () => {
-    setSearchQuery("");
+      setSearchQuery("");
    };
-  return (
-    <>
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-      <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
 
-        <Searchbar
-          value={searchQuery}
-          onChange={({target}) =>{
-            setSearchQuery(target.value);
-          }}
-          handleSearch={handleSearch}
-          onClearSearch={onClearSearch}
-        />
+   return (
+      <>
+         <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+            <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
 
-      <Profileinfo onLogout={onLogout} />
-    </div>
-     
-    </>
-  )
+            <Searchbar
+               value={searchQuery}
+               onChange={(e) => setSearchQuery(e.target.value)} 
+               handleSearch={handleSearch}
+               onClearSearch={onClearSearch}
+            />
+
+            <Profileinfo onLogout={onLogout} />
+         </div>
+      </>
+   );
 }
 
-export default Navbar
+export default Navbar;

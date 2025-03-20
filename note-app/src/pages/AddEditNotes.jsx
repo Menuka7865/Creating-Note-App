fe962ada9c1acab4b92f39d39708a3e52a6d6485
Menuka7865 <menukasandaruwan456@gmail.com>
2025-03-20 23:54:47 +0530

    Taginput page updated

--- note-app/src/pages/AddEditNotes.jsx
+++ note-app/src/pages/AddEditNotes.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TagInput from '../compnents/TagInput';
 
 const AddEditNotes = () => {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [tags,settags] = useState([])
   return (
     <>
     <div>
@@ -11,6 +14,8 @@ const AddEditNotes = () => {
           type='text'
           className='text-2xl text-slate-950 outline-none'
           placeholder='Go to Gym At 5'
+          value={title}
+          onChange={({ target}) => setTitle(target.value)}
         />
       </div>
       <div className='flex flex-col gap-2 mt-4'>
@@ -20,12 +25,14 @@ const AddEditNotes = () => {
           className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
           placeholder='Content'
           rows={10}
+          value={content}
+          onChange={({ target}) => setContent(target.value)}
         />
 
       </div>
        <div className='mt-3'>
          <label className="input-label">TAGS</label>
-         <TagInput/>
+         <TagInput tags = {tags} settags={set}/>
        </div>
        <button className='btn-primary font-medium mt-5 p-3' onClick={()=>{}}>
           ADD

59a312c399257e1b4e585afa70d490f9aa62d7c0
Menuka7865 <menukasandaruwan456@gmail.com>
2025-04-24 23:51:38 +0530

    database connection backend completed

--- note-app/src/pages/Home.jsx
+++ note-app/src/pages/Home.jsx
@@ -49,6 +49,8 @@ function Home() {
         className = "w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
       >
       <AddEditNotes
+      type={openAddEditModal.type}
+      noteData={openAddEditModal.data}
       onclose={()=>{
         setOpenAddEditModal({ isShown:false,type: "add",data:null})
       }}/>

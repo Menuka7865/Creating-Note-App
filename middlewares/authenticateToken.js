1ca52be0568dface9b6031e3a51a999d72ea7232
Menuka7865 <menukasandaruwan456@gmail.com>
2025-05-04 11:15:22 +0530

    authentication token.js file uploaded

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -4,11 +4,14 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    return res.status(403).json({ error: true, message: "Forbidden: No token provided" });
+  }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user; 
+    if (err) return res.status(403).json({ error: true, message: "Forbidden: Invalid token" });
+
+    req.user = user;
     next();
   });
 }
